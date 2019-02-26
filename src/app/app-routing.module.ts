26ce44efc207dd2e54c690aed4f02cb6e734b79e
vadlathalaragava <vadlathalaragava@gmail.com>
2019-02-26 11:37:28 +0530

    login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component'
+import{ HomepageComponent } from './homepage/homepage.component'
+import {LoginComponent} from './login/login.component'
+import {LogoutComponent} from './logout/logout.component'
+const routes: Routes = [
+   {
+     path:"login",
+     component:LoginComponent
+   },
+   {
+     path:"logout",
+    component:LogoutComponent
+   },
+   {
+     path:"homepage",
+     component:HomepageComponent
+   },
+   {
+     path:"dashboard",
+     component:DashboardComponent
+   },
+   {
+     path:"",
+     redirectTo:"/login",
+     pathMatch:"full"
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
